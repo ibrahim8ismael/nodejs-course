270ebce496ca978df95556e514f8fce35fbadd65
ibrahim <ibrahim.ismael@gmail.com>
2025-04-29 20:47:41 +0300

    Add nodmon for real time changes

---
+++ chapter-one/conmands.md
@@ -0,0 +1,3 @@
+npm init // Create package.json
+npm install express // Install express
+npm install --save-dev nodemon  // Real time monitoring
--- chapter-one/server.js
+++ chapter-one/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+console.log('This the test line');
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
