3c94d7ef641d4dd8db952043d610334782f2735d
ibrahim <ibrahim.ismael@gmail.com>
2025-04-30 03:09:30 +0300

    Test.rest

--- chapter-one/conmands.md
+++ chapter-one/conmands.md
@@ -1,3 +1,4 @@
 npm init // Create package.json
 npm install express // Install express
 npm install --save-dev nodemon  // Real time monitoring
+install rest client from Extensions
--- chapter-one/server.js
+++ chapter-one/server.js
@@ -1,9 +1,31 @@
+// The address of the server is: 
+// URL -> http://localhost:3000
+// IP ->  127.0.0.1:3000
 const express = require('express');
 const app = express();
 const port = 3000;
 
 
-console.log('This the test line');
+let data = [
+    {
+        id: 1,
+        name: 'John Doe',
+        age: 30,
+    }
+]
+
+// Set the Website endpoints here
+app.get('/', (req, res) => {
+    res.send('Hello World from the Get!!!');
+})
+app.get('/admin', (req, res) => {
+    res.send('Hello World from the Admin!!!');
+})
+
+// Set the API endpoints here
+app.get('/api/data', (req, res) => {
+    res.send(data);
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
